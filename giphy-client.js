10b4a6475cd5cc9732422feaa4467e5607e53a21
avner.sorek <avner.sorek@env0.com>
2022-10-13 11:03:52 +0300

    oi

--- giphy-client.js
+++ giphy-client.js
@@ -1,18 +1,21 @@
 const axios = require("axios");
 
-const searchForGif = async (text) => {
+const searchForGif = async ({ text, index }) => {
   const { data: giphySearchResponse } = await axios.get(
     "https://api.giphy.com/v1/gifs/search",
     {
       params: {
         api_key: "yf0xX299HlpDo760hrbWL99dBeQiWnPf",
         q: text,
+        offset: index,
         limit: 1,
       },
     }
   );
 
-  return giphySearchResponse.data[0].images.downsized.url;
+  const { images } = giphySearchResponse.data[0];
+
+  return { sendUrl: images.original.url, previewUrl: images.downsized.url };
 };
 
 // const getNextGif;
--- index.js
+++ index.js
@@ -16,103 +16,124 @@ const app = new App({
   },
 });
 
-app.command("/gif0", async ({ ack, say, body, ...props }) => {
-  // console.log("got request", props);
-  await ack({ text: "Getting your gifs ..." });
+const getGifPost = async ({ text, index }) => {
+  const { sendUrl, previewUrl } = await searchForGif({ text, index });
+  return getPostWithButtons({ text, index, sendUrl, previewUrl });
+};
 
-  const gifUrl = await searchForGif(body.text);
-
-  // TODO: all the "says" are public!!
-  const secondPost = {
-    blocks: [
-      {
-        type: "image",
-        title: {
-          type: "plain_text",
-          text: "Please enjoy this photo of a " + body.text,
-        },
-        block_id: "image4",
-        image_url: gifUrl,
-        alt_text: body.text,
-      },
-      {
-        type: "actions",
-        block_id: "actions1",
-        elements: [
-          {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Next",
-            },
-            value: JSON.stringify({ text: body.text, index: 0 }),
-            action_id: "next_button",
-          },
-          {
-            // Prev should be disabled on first gif :/
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Prev",
-            },
-            value: "prev",
-            action_id: "prev_button",
-          },
-          {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Send",
-            },
-            value: "send",
-            action_id: "send_button",
-          },
-          {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Cancel",
-            },
-            value: "cancel",
-            action_id: "cancel_button",
-          },
-        ],
-      },
-    ],
-  };
-
-  await say(secondPost);
+app.command("/gif0", async ({ ack, respond, body, client }) => {
+  await ack({ text: "Getting your gif ..." });
+  const { text, channel_id, user_id } = body;
+  const firstPost = await getGifPost({ text, index: 0 });
+  await client.chat.postEphemeral({
+    channel: channel_id,
+    user: user_id,
+    ...firstPost,
+  });
 });
 
-app.action("next_button", async ({ ack, say, body }) => {
-  // Acknowledge action request
+app.action("next_button", async ({ ack, respond, body, ...props }) => {
   const { text, index } = JSON.parse(body.actions[0].value);
-  console.log(text, index);
   await ack();
-  await say("Request approved 👍");
+  const nextPost = await getGifPost({ text, index });
+  // await respond(nextPost);
+  await client.chat.postEphemeral({
+    channel: body.channel_id,
+    user: body.user_id,
+    ...nextPost,
+  });
 });
 
-app.action("prev_button", async ({ ack, say }) => {
-  // Acknowledge action request
+app.action("prev_button", async ({ ack, respond, body }) => {
+  const { text, index } = JSON.parse(body.actions[0].value);
   await ack();
-  await say("Request approved 👍");
+  const nextPost = await getGifPost({ text, index });
+  await respond(nextPost);
 });
 
-app.action("send_button", async ({ ack, say }) => {
-  // Acknowledge action request
+app.action("send_button", async ({ ack, body, say }) => {
+  const { text, sendUrl } = JSON.parse(body.actions[0].value);
   await ack();
-  await say("Request approved 👍");
+  const publicPost = getImagePost({ text, url: sendUrl });
+  // TODO: Send as
+  await say({ ...publicPost });
 });
 
 app.action("cancel_button", async ({ ack, say }) => {
-  // Acknowledge action request
   await ack();
   await say("Request approved 👍");
 });
 
-(async () => {
-  // Start the app
-  await app.start(process.env.PORT || 3000);
+const getImagePost = ({ text, url }) => ({
+  text,
+  blocks: [
+    {
+      type: "image",
+      title: { type: "plain_text", text },
+      block_id: "image4",
+      image_url: url,
+      alt_text: text,
+    },
+  ],
+});
 
-  console.log("⚡️ Bolt app is running!");
-})();
+const getPostWithButtons = ({ text, index, sendUrl, previewUrl }) => {
+  const post = getImagePost({ text, url: previewUrl });
+  post.blocks.push({
+    type: "actions",
+    block_id: "actions1",
+    elements: [
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "Prev",
+        },
+        value: JSON.stringify({ text, index: Math.max(0, index - 1) }),
+        action_id: "prev_button",
+      },
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "Next",
+        },
+        value: JSON.stringify({ text, index: index + 1 }),
+        action_id: "next_button",
+      },
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "Send",
+        },
+        value: JSON.stringify({ text, sendUrl }),
+        action_id: "send_button",
+        style: "primary",
+      },
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "Cancel",
+        },
+        value: "cancel",
+        action_id: "cancel_button",
+        style: "danger",
+      },
+    ],
+  });
+  return post;
+};
+
+const start = async () => {
+  try {
+    await app.start(process.env.PORT || 3000);
+    console.log("⚡️ Bolt app is running!");
+  } catch (ex) {
+    console.error(ex);
+    process.exit(-1);
+  }
+};
+
+start();
